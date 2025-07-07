51b4cc778681a00c3f35f863726cdba136b01fee
s1f10230101 <s1f102301011@iniad.org>
2025-07-07 18:56:07 +0900

    add: getpostテストの追加(Red)

--- backend-apigate/cmd/server/main_test.go
+++ backend-apigate/cmd/server/main_test.go
@@ -43,3 +43,32 @@ func TestCreatePost(t *testing.T) {
 			rr.Body.String(), `"title":"Test Post"`)
 	}
 }
+
+// テストの関数名は、Testから始まっていれば日本語でも大丈夫です。
+func TestGetPosts_ポストの全要素を返すAPIテスト(t *testing.T) {
+	router := NewTestRouter()
+
+	// まずはポストを作成しておく
+	postJSON := `{"title":"Test Post","content":"This is a test post."}`
+	reqCreate, _ := http.NewRequest("POST", "/v1/posts", bytes.NewBufferString(postJSON))
+	reqCreate.Header.Set("Content-Type", "application/json")
+	router.ServeHTTP(httptest.NewRecorder(), reqCreate)
+
+	// GETリクエストを作成
+	req, err := http.NewRequest("GET", "/v1/posts", nil)
+	if err != nil {
+		t.Fatalf("Could not create request: %v", err)
+	}
+	// レスポンスを記録
+	rr := httptest.NewRecorder()
+	router.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got \033[31m%v\033[0m want %v",
+			status, http.StatusOK)
+	}
+	if !bytes.Contains(rr.Body.Bytes(), []byte("Test Post")) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), `"title":"Test Post"`)
+	}
+}
